9c3bfbb27c278e9c929fb7d7426ba7eec83de082
Rafael Alonso <rafaelalonsobarbosa@gmail.com>
2023-09-08 16:47:11 -0300

    feat(init)
    iniciado backend da API do mariah

---
+++ backend/mariah-api/src/app.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { ProductsModule } from './products/products.module';
+import { SalesModule } from './sales/sales.module';
+
+@Module({
+  imports: [UserModule, ProductsModule, SalesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
